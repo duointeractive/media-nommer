1284a6be6f57807a3c0ef1b5138cb8b80010f3e8
Greg Taylor <gtaylor@duointeractive.com>
2010-12-21 13:55:02 -0500

    Getting stuff together we need for packaging and distribution.

---
+++ README.rst
@@ -0,0 +1,27 @@
+media-nommer
+============
+
+media-nommer is a Python-based media encoding system, using Amazon AWS
+as its backbone. 
+
+
+Status
+------
+
+media-nommer is currently in very early development. At this time, the
+software is probably only appropriate for those with a strong grasp on
+Python and Amazon AWS.
+  
+Documentation
+-------------
+
+Documentation may be found on our `website`_.
+
+.. _website: http://duointeractive.github.com/media-nommer/
+  
+License
+-------
+
+media-nommer is licensed under the `BSD License`_.
+
+.. _BSD License: https://github.com/duointeractive/media-nommer/blob/master/LICENSE
---
+++ setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+import media_nommer
+
+long_description = open('README.rst').read()
+
+setup(
+    name='media-nommer',
+    version=media_nommer.VERSION,
+    packages=['media_nommer'],
+    description='A Python-based media encoding system, using Amazon AWS as its backbone.',
+    long_description=long_description,
+    author='DUO Interactive, LLC',
+    author_email='gtaylor@duointeractive.com',
+    license='BSD License',
+    url='http://duointeractive.github.com/media-nommer/',
+    platforms=["any"],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Environment :: Web Environment',
+        'Topic :: Multimedia :: Video :: Conversion',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
