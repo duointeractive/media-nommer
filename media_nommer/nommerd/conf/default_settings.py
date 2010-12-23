671ab30b684cd42e9f7422c49f38d6d7374564f9
Greg Taylor <gtaylor@duointeractive.com>
2010-12-23 13:07:21 -0500

    Comments for default setting and unit tests for settings.

--- media_nommer/nommerd/conf/default_settings.py
+++ media_nommer/nommerd/conf/default_settings.py
@@ -1,1 +1,6 @@
+"""
+This file contains the global default configuration for nommerd. When the
+daemon starts, these are loaded as initial values, then the user's settings
+override anything here.
+"""
 TEST_SETTING = 'yay'
---
+++ media_nommer/nommerd/conf/tests.py
@@ -0,0 +1,22 @@
+import unittest
+from media_nommer.nommerd.conf import SettingsStore
+from media_nommer.nommerd.conf import default_settings
+
+class SettingsTestTest(unittest.TestCase):
+    def setUp(self):
+        self.settings = SettingsStore()
+
+    def test_overriding(self):
+        """
+        Tests overriding default settings with the user-specified settings.
+        """
+        # Pretend that this is the global default for some arbitrary setting.
+        self.settings.SOME_SETTING = 1
+        # Pretend like this is the user's settings module.
+        user_settings = SettingsStore()
+        # The user has changed some setting to a non-default value.
+        user_settings.SOME_SETTING = 2
+        # Now load the user's settings over the defaults, like the daemon does.
+        self.settings.update_settings_from_module(user_settings)
+        # Make sure the new setting matches the user's values.
+        self.assert_(self.settings.SOME_SETTING == user_settings.SOME_SETTING)
