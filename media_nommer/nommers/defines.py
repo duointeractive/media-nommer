a3711587c62f31b0909fb6a5d2f0ce46abbc6b0f
Greg Taylor <gtaylor@duointeractive.com>
2010-12-29 15:13:47 -0500

    Commenting in nommer defines.

--- media_nommer/nommers/defines.py
+++ media_nommer/nommers/defines.py
@@ -1,4 +1,9 @@
 """
 Some global constant definitions. These should not be modified.
 """
+
+"""
+The following are used as key values for job states in workflow job state DBs.
+"""
+# Jobs in this state have been detected but not acted upon yet.
 JOB_STATE_PENDING = 'PENDING'
