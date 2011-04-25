f965b840bc82c3c47ed95af9981c3191a5669adc
Greg Taylor <gtaylor@duointeractive.com>
2011-04-25 15:13:33 -0400

    Guess it'd help if I commit that exception I used.

---
+++ media_nommer/core/storage_backends/exceptions.py
@@ -0,0 +1,20 @@
+"""
+Storage backend exceptions
+"""
+class StorageException(Exception):
+    """
+    A generic Storage backend-related exception. Try to be more specific in your 
+    code, just use this as a parent class.
+    """
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return repr(self.message)
+
+class InfileNotFoundException(StorageException):
+    """
+    Raised by storage backends when the given source_path (the media to encode)
+    cannot be found. 
+    """
+    message = "The specified input file cannot be found."
