6bf9147efd6fa2d94d6eb49cb84e69ec341556f8
Greg Taylor <gtaylor@duointeractive.com>
2011-10-11 17:35:02 -0400

    Removing dependence on boto2.4b

--- setup.py
+++ setup.py
@@ -31,8 +31,7 @@ setup(
     url='http://duointeractive.github.com/media-nommer/',
     platforms=["any"],
     # Can't use this until pip install --upgrade behavior is corrected.
-    #install_requires=['boto==2.0b4', 'twisted', 'txrestapi', 'simplejson'],
-    install_requires=['boto==2.0b4', 'simplejson'],
+    install_requires=['boto', 'simplejson'],
     provides=['media_nommer'],
     packages=[
         'media_nommer',
