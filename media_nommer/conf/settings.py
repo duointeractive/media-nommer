75b372e10be40fdaef8ca8b5c66d8611e76a3646
Greg Taylor <gtaylor@duointeractive.com>
2011-12-20 11:21:23 -0500

    Changing nommer AMI.

--- media_nommer/conf/settings.py
+++ media_nommer/conf/settings.py
@@ -68,7 +68,7 @@ EC2_SECURITY_GROUPS = ['media_nommer']
 """Default: ``['media_nommer']``
 
 The AWS_ security groups to create EC2_ instances under."""
-EC2_AMI_ID = 'ami-1922ee70'
+EC2_AMI_ID = 'ami-09f42160'
 """Default: ``The latest upstream AMI compatible with this git revision.``
 
 The AMI ID for the media-nommer EC2_ instance."""
